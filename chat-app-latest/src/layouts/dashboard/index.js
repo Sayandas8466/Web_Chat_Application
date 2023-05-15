6e6482763d02031e556012df3c69825332f60c27
Sayan <django.demo.sayan01@gmail.com>
2023-05-15 11:42:26 +0530

    Initial Commit

---
+++ chat-app-latest/src/layouts/dashboard/index.js
@@ -0,0 +1,180 @@
+import React, { useEffect } from "react";
+import { Stack } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+import useResponsive from "../../hooks/useResponsive";
+import SideNav from "./SideNav";
+import { useDispatch, useSelector } from "react-redux";
+import { FetchUserProfile, SelectConversation, showSnackbar } from "../../redux/slices/app";
+import { socket, connectSocket } from "../../socket";
+import {
+  UpdateDirectConversation,
+  AddDirectConversation,
+  AddDirectMessage,
+} from "../../redux/slices/conversation";
+import AudioCallNotification from "../../sections/Dashboard/Audio/CallNotification";
+import VideoCallNotification from "../../sections/Dashboard/video/CallNotification";
+import {
+  PushToAudioCallQueue,
+  UpdateAudioCallDialog,
+} from "../../redux/slices/audioCall";
+import AudioCallDialog from "../../sections/Dashboard/Audio/CallDialog";
+import VideoCallDialog from "../../sections/Dashboard/video/CallDialog";
+import { PushToVideoCallQueue, UpdateVideoCallDialog } from "../../redux/slices/videoCall";
+
+const DashboardLayout = () => {
+  const isDesktop = useResponsive("up", "md");
+  const dispatch = useDispatch();
+  const {user_id} = useSelector((state) => state.auth);
+  const { open_audio_notification_dialog, open_audio_dialog } = useSelector(
+    (state) => state.audioCall
+  );
+  const { open_video_notification_dialog, open_video_dialog } = useSelector(
+    (state) => state.videoCall
+  );
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { conversations, current_conversation } = useSelector(
+    (state) => state.conversation.direct_chat
+  );
+
+  useEffect(() => {
+    dispatch(FetchUserProfile());
+  }, []);
+  
+
+  const handleCloseAudioDialog = () => {
+    dispatch(UpdateAudioCallDialog({ state: false }));
+  };
+  const handleCloseVideoDialog = () => {
+    dispatch(UpdateVideoCallDialog({ state: false }));
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }
+      };
+
+      window.onload();
+
+      if (!socket) {
+        connectSocket(user_id);
+      }
+
+      socket.on("audio_call_notification", (data) => {
+        // TODO => dispatch an action to add this in call_queue
+        dispatch(PushToAudioCallQueue(data));
+      });
+      
+      socket.on("video_call_notification", (data) => {
+        // TODO => dispatch an action to add this in call_queue
+        dispatch(PushToVideoCallQueue(data));
+      });
+
+      socket.on("new_message", (data) => {
+        const message = data.message;
+        console.log(current_conversation, data);
+        // check if msg we got is from currently selected conversation
+        if (current_conversation?.id === data.conversation_id) {
+          dispatch(
+            AddDirectMessage({
+              id: message._id,
+              type: "msg",
+              subtype: message.type,
+              message: message.text,
+              incoming: message.to === user_id,
+              outgoing: message.from === user_id,
+            })
+          );
+        }
+      });
+
+      socket.on("start_chat", (data) => {
+        console.log(data);
+        // add / update to conversation list
+        const existing_conversation = conversations.find(
+          (el) => el?.id === data._id
+        );
+        if (existing_conversation) {
+          // update direct conversation
+          dispatch(UpdateDirectConversation({ conversation: data }));
+        } else {
+          // add direct conversation
+          dispatch(AddDirectConversation({ conversation: data }));
+        }
+        dispatch(SelectConversation({ room_id: data._id }));
+      });
+
+      socket.on("new_friend_request", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "New friend request received",
+          })
+        );
+      });
+
+      socket.on("request_accepted", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "Friend Request Accepted",
+          })
+        );
+      });
+
+      socket.on("request_sent", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+    }
+
+    // Remove event listener on component unmount
+    return () => {
+      socket?.off("new_friend_request");
+      socket?.off("request_accepted");
+      socket?.off("request_sent");
+      socket?.off("start_chat");
+      socket?.off("new_message");
+      socket?.off("audio_call_notification");
+    };
+  }, [isLoggedIn, socket]);
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/auth/login"} />;
+  }
+
+  return (
+    <>
+      <Stack direction="row">
+        {isDesktop && (
+          // SideBar
+          <SideNav />
+        )}
+
+        <Outlet />
+      </Stack>
+      {open_audio_notification_dialog && (
+        <AudioCallNotification open={open_audio_notification_dialog} />
+      )}
+      {open_audio_dialog && (
+        <AudioCallDialog
+          open={open_audio_dialog}
+          handleClose={handleCloseAudioDialog}
+        />
+      )}
+      {open_video_notification_dialog && (
+        <VideoCallNotification open={open_video_notification_dialog} />
+      )}
+      {open_video_dialog && (
+        <VideoCallDialog
+          open={open_video_dialog}
+          handleClose={handleCloseVideoDialog}
+        />
+      )}
+    </>
+  );
+};
+
+export default DashboardLayout;
---
+++ chat-app-latest/src/pages/dashboard/Call.js
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  Link,
+} from "@mui/material";
+import { MagnifyingGlass, Phone } from "phosphor-react";
+import React, { useEffect, useState } from "react";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+
+import { useTheme } from "@mui/material/styles";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+import { CallLogElement } from "../../components/CallElement";
+import StartCall from "../../sections/Dashboard/StartCall";
+import { useDispatch, useSelector } from "react-redux";
+import { FetchCallLogs } from "../../redux/slices/app";
+
+const Call = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchCallLogs());
+  }, []);
+  const { call_logs } = useSelector((state) => state.app);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+  const theme = useTheme();
+  return (
+    <>
+      <Stack direction="row" sx={{ width: "100%" }}>
+        {/* Left */}
+
+        <Box
+          sx={{
+            overflowY: "scroll",
+
+            height: "100vh",
+            width: 340,
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+            <Stack
+              alignItems={"center"}
+              justifyContent="space-between"
+              direction="row"
+            >
+              <Typography variant="h5">Call Log</Typography>
+            </Stack>
+
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Stack>
+
+            <Stack
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              direction={"row"}
+            >
+              <Typography variant="subtitle2" sx={{}} component={Link}>
+                Start a conversation
+              </Typography>
+              <IconButton onClick={handleOpenDialog}>
+                <Phone style={{ color: theme.palette.primary.main }} />
+              </IconButton>
+            </Stack>
+            <Divider />
+            <Stack sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}>
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.4}>
+                  {call_logs.map((el, idx) => {
+                    return <CallLogElement key={idx} {...el} />;
+                  })}
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+      {openDialog && (
+        <StartCall open={openDialog} handleClose={handleCloseDialog} />
+      )}
+    </>
+  );
+};
+
+export default Call;
